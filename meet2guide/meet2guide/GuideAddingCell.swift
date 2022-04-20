cf432b7d3dc6895e0d732ccc333a859827c00d0a
petroleumisbooks <petroleumisbooks@gmail.com>
2022-04-20 14:48:08 +0300

    MEET-13 Окно добавления экскурсии

--- meet2guide/meet2guide/GuideAddingCell.swift
+++ meet2guide/meet2guide/GuideAddingCell.swift
@@ -1,8 +1,63 @@
-//
-//  GuideAddingCell.swift
-//  meet2guide
-//
-//  Created by Сэймур Гусейнов on 19.04.2022.
-//
+import UIKit
+import PinLayout
 
-import Foundation
+class GuideAddingCell: UITableViewCell {
+    private let titleLabel: UILabel = UILabel()
+    
+    private var information: UITextField = UITextField()
+    
+    private var colorBlue: UIColor = UIColor(red: 52 / 255, green: 94 / 255, blue: 202 / 255, alpha: 100)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpCell()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel
+            .pin
+            .left(20)
+            .top(self.contentView.frame.height / 2 - 12.5)
+            .width(100)
+            .height(25)
+        
+        information
+            .pin
+            .left(150)
+            .top(self.contentView.frame.height / 2 - 20)
+            .width(200)
+            .height(40)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpCell() {
+        titleLabel.text = "test"
+        titleLabel.font = UIFont(name: "Montserrat-Medium", size: 14)
+        titleLabel.textColor = colorBlue
+        titleLabel.textAlignment = .left
+        
+        information.text = "test"
+        information.font = UIFont(name: "Montserrat-Medium", size: 14)
+        information.textColor = colorBlue
+        information.textAlignment = .left
+        
+        information.layer.borderColor = CGColor(red: 52 / 255, green: 94 / 255, blue: 202 / 255, alpha: 100)
+        information.layer.borderWidth = 1.0
+        information.borderStyle = .roundedRect
+        information.layer.masksToBounds = true
+        information.layer.cornerRadius = 10
+        
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(information)
+    }
+    
+    func configure(with config: guideInfo) {
+        titleLabel.text = config.tableLabel
+        information.text = config.textIn
+    }
+    
+}
--- meet2guide/meet2guide/GuideAddingViewController.swift
+++ meet2guide/meet2guide/GuideAddingViewController.swift
@@ -1,8 +1,12 @@
-
 import Foundation
 import UIKit
 import PinLayout
 
+struct guideInfo{
+    let tableLabel: String?
+    var textIn: String?
+    
+}
 
 class GuideAddingViewController: UIViewController {
     private var textTitle: String? = "Добавить экскурсию"
@@ -14,7 +18,12 @@ class GuideAddingViewController: UIViewController {
     private let addPhotoButton: UIButton = UIButton()
     private let addGuideButton: UIButton = UIButton()
     private let colorBlueCustom: UIColor = UIColor(red: 0.205, green: 0.369, blue: 0.792, alpha: 1)
-    private var imagesFrame: UIImage? = UIImage()
+    private let imagesFrame: UIImageView = UIImageView()
+    private var tableViewGuideInfo: UITableView = UITableView()
+    private var guideConfiguration: [guideInfo] = [guideInfo(tableLabel: "Название", textIn: "Достопримечательности Москвы"), guideInfo(tableLabel: "Дата", textIn: "10.04.2003"), guideInfo(tableLabel: "Адрес", textIn: "Красная пл. 1, г. Москва"), guideInfo(tableLabel: "Описание", textIn: "траляля, тополя")]
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +42,8 @@ class GuideAddingViewController: UIViewController {
         
         
         configButton(addGuideButton, "Добавить", colorBlueCustom, .white)
+        configImages()
+        setUpTableViewGuideInfo()
         
         
         
@@ -42,21 +53,58 @@ class GuideAddingViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         
-        titleLabel.pin
+        titleLabel
+            .pin
             .top(view.safeAreaInsets.top)
             .left(5%)
             .right(5%)
             .sizeToFit(.width)
         
-        addGuideButton.pin
+        imagesFrame
+            .pin
+            .topCenter(to: titleLabel.anchor.bottomCenter).margin(30)
+            .height(180)
+            .width(90%)
+            .hCenter()
+        
+        addGuideButton
+            .pin
             .bottom(view.safeAreaInsets.bottom + 20)
             .right(5%)
             .left(5%)
             .width(90%)
-            .height(50%)
-            .maxHeight(40)
-
+            .height(60%)
+            .maxHeight(50)
+        
+        tableViewGuideInfo
+            .pin
+            .topCenter(to: imagesFrame.anchor.bottomCenter)
+            .margin(10)
+            .width((self.view.window?.frame.width ?? 310))
+            .height((self.view.window?.frame.height ?? 310) - 600)
+     
     }
+    
+    private func configImages() {
+        imagesFrame.image = UIImage(named: "moscow")
+        imagesFrame.layer.cornerRadius = 10
+        imagesFrame.clipsToBounds = true
+        
+        view.addSubview(imagesFrame)
+    }
+    
+    private func setUpTableViewGuideInfo() {
+        tableViewGuideInfo.frame = view.bounds
+        
+        tableViewGuideInfo.delegate = self
+        tableViewGuideInfo.dataSource = self
+        tableViewGuideInfo.register(GuideAddingCell.self, forCellReuseIdentifier: "GuideAddingCell")
+        
+        tableViewGuideInfo.separatorStyle = .none
+        
+        view.addSubview(tableViewGuideInfo)
+    }
+
     
     
     func configButton (_ button: UIButton, _ name: String, _ backGrColor: UIColor, _ textColor: UIColor){
@@ -65,7 +113,7 @@ class GuideAddingViewController: UIViewController {
         button.setTitleColor(textColor, for: .normal)
         button.contentHorizontalAlignment = .center
         button.titleLabel?.font =  UIFont(name: "Avenir", size: 15)
-        button.layer.cornerRadius = 5
+        button.layer.cornerRadius = 12
         button.layer.borderWidth = 2.0
         button.layer.borderColor = (colorBlueCustom).cgColor
 
@@ -75,7 +123,26 @@ class GuideAddingViewController: UIViewController {
         button.layer.shadowOpacity = 1
         button.clipsToBounds = true
         button.layer.masksToBounds = false
+        
         view.addSubview(button)
     }
 }
 
+extension GuideAddingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GuideAddingCell", for: indexPath) as? GuideAddingCell
+        
+        cell?.configure(with: guideConfiguration[indexPath.row])
+        
+        cell?.selectionStyle = UITableViewCell.SelectionStyle.none
+        return cell ?? .init()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+}
