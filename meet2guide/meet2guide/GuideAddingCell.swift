e824e5c13616546cfab624fbf62d0cf38148ff47
petroleumisbooks <petroleumisbooks@gmail.com>
2022-04-24 14:21:40 +0300

    Окно добавления экскурсии

--- meet2guide/meet2guide/GuideAddingCell.swift
+++ meet2guide/meet2guide/GuideAddingCell.swift
@@ -1,29 +1,63 @@
-//
-//  GuideAddingCell.swift
-//  meet2guide
-//
-//  Created by Сэймур Гусейнов on 24.04.2022.
-//
-
 import UIKit
+import PinLayout
 
-class GuideAddingCell: UIViewController {
+class GuideAddingCell: UITableViewCell {
+    private let titleLabel: UILabel = UILabel()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private var information: UITextField = UITextField()
 
-        // Do any additional setup after loading the view.
+    private var colorBlue: UIColor = UIColor(red: 52 / 255, green: 94 / 255, blue: 202 / 255, alpha: 100)
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpCell()
     }
-    
 
-    /*
-    // MARK: - Navigation
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel
+            .pin
+            .left(20)
+            .top(self.contentView.frame.height / 2 - 12.5)
+            .width(100)
+            .height(25)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        information
+            .pin
+            .left(150)
+            .top(self.contentView.frame.height / 2 - 20)
+            .width(200)
+            .height(40)
     }
-    */
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setUpCell() {
+        titleLabel.text = "test"
+        titleLabel.font = UIFont(name: "Montserrat-Medium", size: 14)
+        titleLabel.textColor = colorBlue
+        titleLabel.textAlignment = .left
+
+        information.text = "test"
+        information.font = UIFont(name: "Montserrat-Medium", size: 14)
+        information.textColor = colorBlue
+        information.textAlignment = .left
+
+        information.layer.borderColor = CGColor(red: 52 / 255, green: 94 / 255, blue: 202 / 255, alpha: 100)
+        information.layer.borderWidth = 1.0
+        information.borderStyle = .roundedRect
+        information.layer.masksToBounds = true
+        information.layer.cornerRadius = 10
+
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(information)
+    }
+
+    func configure(with config: guideInfo) {
+        titleLabel.text = config.tableLabel
+        information.text = config.textIn
+    }
 
 }
