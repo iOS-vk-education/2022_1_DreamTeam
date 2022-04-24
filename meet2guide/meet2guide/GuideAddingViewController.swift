492e7cfda5028040572db0056feac8e45db4eea5
petroleumisbooks <petroleumisbooks@gmail.com>
2022-04-24 13:41:35 +0300

    Окно добавления экскурсии

---
+++ meet2guide/meet2guide/GuideAddingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GuideAddingViewController.swift
+//  meet2guide
+//
+//  Created by Сэймур Гусейнов on 24.04.2022.
+//
+
+import UIKit
+
+class GuideAddingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
